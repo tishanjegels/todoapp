186221269186cc9dde5f6572882dc6037084da78
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-06-13 14:19:16 +0200

    edits

---
+++ js/todoapp.js
@@ -0,0 +1,17 @@
+const clear = document.querySelector(".clear");
+
+
+
+const list = document.getElementById("list");
+
+function addToDo(toDo) {
+    const text = <list class="item">
+
+        <i class="co fa fa-circle-thin" job="complete"></i>
+        <p class="text">${toDo}</p>
+        <i class="de fa fa-trash-o" job="delete"></i>
+        
+        </list>
+    const position = "beforend";
+        
+}
